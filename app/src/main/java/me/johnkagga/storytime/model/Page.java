0bde096f6fbe7c557113e9e9aa393b440e5f3e6e
jokamjohn <johnkagga@gmail.com>
2015-06-14 23:09:32 +0300

    Added another constructor in Page class

--- app/src/main/java/me/johnkagga/storytime/model/Page.java
+++ app/src/main/java/me/johnkagga/storytime/model/Page.java
@@ -8,12 +8,21 @@ public class Page {
     private String mText;
     private Choice mChoice1;
     private Choice mChoice2;
+    private boolean mIsFinal = false; //boolean flag to show the last or ongoing stories
 
     public Page(int imageId, String text, Choice choice1, Choice choice2) {
         mImageId = imageId;
         mText = text;
         mChoice1 = choice1;
         mChoice2 = choice2;
+    }
+
+    public Page(int imageId, String text) {
+        mImageId = imageId;
+        mText = text;
+        mChoice1 = null;
+        mChoice2 =null;
+        mIsFinal = true;
     }
 
     public int getImageId() {
--- app/src/main/java/me/johnkagga/storytime/model/Story.java
+++ app/src/main/java/me/johnkagga/storytime/model/Story.java
@@ -22,5 +22,34 @@ public class Story {
                 "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. Even stranger, it's calling you by name: \\\"Help me, %1$s, you're my only hope.\\\"",
                 new Choice("Stop and Investigate", 1),
                 new Choice("Continue home to earth",2));
+
+        mPages[1] = new Page(R.drawable.page0,
+                "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. Even stranger, it's calling you by name: \\\"Help me, %1$s, you're my only hope.\\\"",
+                new Choice("Explore the cave", 3),
+                new Choice("Explore the rover", 4));
+
+        mPages[2] = new Page(R.drawable.page0,
+                "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. Even stranger, it's calling you by name: \\\"Help me, %1$s, you're my only hope.\\\"",
+                new Choice("Head back to Mars to investigate", 4),
+                new Choice("Continue home to Earth", 6));
+
+        mPages[3] = new Page(R.drawable.page0,
+                "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. Even stranger, it's calling you by name: \\\"Help me, %1$s, you're my only hope.\\\"",
+                new Choice("Refill at ship and explore the rover", 4),
+                new Choice("Continue towards the faint light", 5));
+
+        mPages[4] = new Page(R.drawable.page0,
+                "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. Even stranger, it's calling you by name: \\\"Help me, %1$s, you're my only hope.\\\"",
+                new Choice("Explore the coordinates", 5),
+                new Choice("Return to Earth", 6));
+
+        mPages[5] = new Page(R.drawable.page0,
+                "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. Even stranger, it's calling you by name: \\\"Help me, %1$s, you're my only hope.\\\""
+               );
+
+        mPages[6] = new Page(R.drawable.page0,
+                "On your return trip from studying Saturn's rings, you hear a distress signal that seems to be coming from the surface of Mars. It's strange because there hasn't been a colony there in years. Even stranger, it's calling you by name: \\\"Help me, %1$s, you're my only hope.\\\""
+               );
+
     }
 }
