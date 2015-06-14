cad3e3f3f999a5a2b50b7d6c3baa21206ec91974
jokamjohn <johnkagga@gmail.com>
2015-06-14 18:29:39 +0300

    Added a listener

--- app/src/main/java/me/johnkagga/storytime/MainActivityFragment.java
+++ app/src/main/java/me/johnkagga/storytime/MainActivityFragment.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    private EditText mEditText;
+    private Button mButton;
 
     public MainActivityFragment() {
     }
@@ -18,6 +23,19 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootVeiw = inflater.inflate(R.layout.fragment_main, container, false);
+
+        mEditText = (EditText)rootVeiw.findViewById(R.id.editText);
+        mButton = (Button)rootVeiw.findViewById(R.id.startbutton);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = mEditText.getText().toString();
+                Toast.makeText(getActivity(),name,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootVeiw;
     }
 }
